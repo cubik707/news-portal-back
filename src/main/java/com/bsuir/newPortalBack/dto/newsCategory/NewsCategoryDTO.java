ebabcf1fee5e99bfa39b2f3ae9d8ddf0fc5f0d7e
Vladislava <stdemidovez@gmail.com>
2025-05-04 00:03:56 +0300

    feat(newsCategories): create dto's, mappers, service

---
+++ src/main/java/com/bsuir/newPortalBack/dto/newsCategory/NewsCategoryDTO.java
@@ -0,0 +1,13 @@
+package com.bsuir.newPortalBack.dto.newsCategory;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class NewsCategoryDTO {
+  private int id;
+  private String name;
+}
--- src/main/java/com/bsuir/newPortalBack/entities/NewsCategoryEntity.java
+++ src/main/java/com/bsuir/newPortalBack/entities/NewsCategoryEntity.java
@@ -1,8 +1,7 @@
 package com.bsuir.newPortalBack.entities;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -13,6 +12,9 @@ import java.util.Set;
 @Table(name = "news_categories")
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class NewsCategoryEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
